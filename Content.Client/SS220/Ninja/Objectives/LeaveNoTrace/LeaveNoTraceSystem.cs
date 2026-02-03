308a8a22f337b3e4d6972977582296b423ff7415
ReeZer2 <63300653+ReeZer2@users.noreply.github.com>
2026-02-03 13:29:09 +0200

    FIX: Unused ninja objective that cause server error (#3873)

--- Content.Client/SS220/Ninja/Objectives/LeaveNoTrace/LeaveNoTraceSystem.cs
+++ Content.Client/SS220/Ninja/Objectives/LeaveNoTrace/LeaveNoTraceSystem.cs
@@ -69,6 +69,9 @@ public sealed class LeaveNoTraceSystem : SharedLeaveNoTraceSystem
         if (_playerManager.LocalEntity != ent.Owner)
             return;
 
+        if (TerminatingOrDeleted(ent.Owner))
+            return;
+
         _overlay.IsReveal = true;
 
         var fadeComponent = EnsureComp<RevealOverlayFadeComponent>(ent.Owner);
