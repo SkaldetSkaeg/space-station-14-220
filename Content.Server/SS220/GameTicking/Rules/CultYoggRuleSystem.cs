0788e3a1b1baee02aeea50a6fb99b8c08c62d26e
SkaldetSkaeg <impotekh@gmail.com>
2026-02-19 23:35:24 +0700

    tweaks

--- Content.Server/SS220/GameTicking/Rules/CultYoggRuleSystem.cs
+++ Content.Server/SS220/GameTicking/Rules/CultYoggRuleSystem.cs
@@ -579,12 +579,6 @@ public sealed class CultYoggRuleSystem : GameRuleSystem<CultYoggRuleComponent>
         _adminLogger.Add(LogType.RoundFlow, LogImpact.High, $"Cult Yogg progressed to {stage}");
         _chatManager.SendAdminAlert(Loc.GetString("cult-yogg-stage-admin-alert", ("stage", stage)));
 
-        if (TryGetNextStage(rule, out _, out var nextStageDefinition))
-        {
-            //Adding Stage sacrificials for progressing for a next stage
-            TryInitializeNextStageSacrificials(rule, nextStageDefinition);
-        }
-
         //doing stage non-entity-related things
         DoStageEffects(rule, stage);
 
@@ -593,6 +587,11 @@ public sealed class CultYoggRuleSystem : GameRuleSystem<CultYoggRuleComponent>
 
         UpdateStageOnEnts<CultYoggComponent>(ref changeStageEvent, stageDefinition);
         UpdateStageOnEnts<MiGoComponent>(ref changeStageEvent, stageDefinition);
+
+        if (TryGetNextStage(rule, out _, out var nextStageDefinition))//No next stage = no new sacraficials
+        {
+            TryInitializeNextStageSacrificials(rule, nextStageDefinition);
+        }
     }
 
     private void UpdateStageOnEnts<T>(ref ChangeCultYoggStageEvent stageEvent, CultYoggStageDefinition stageDef) where T : IComponent
