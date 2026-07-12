4ecf243e0f6c50025e339e752b7a3c15ca83ee8d
Theywod <159628037+Theywod@users.noreply.github.com>
2026-07-12 18:05:52 +0700

    [ADD] КПБ (#4063)
    * ITS ALIVE!
    
    * fix face menu
    
    from SpriteSpecifier.Rsi to SpriteSpecifier
    
    * data field speed work
    
    * datafield damage emp
    
    * change name drain action
    
    * add networkcomp
    
    * add datafield mark
    
    * Flashlight and language fix
    
    * Fix locale
    
    * Copyright fix
    
    * KPB brain in borg fix
    
    * from kpb to ipc
    
    * NoRainbow Fix
    
    * TemperatureSpeed Delete and PressureAlert Fix
    
    * add copyright and change audio
    
    * Crawling fix
    
    * MorePoint Fix
    
    * add revive after heal from dead state
    
    * delete popup in crit state
    
    * HealingAndCloning Fix
    
    * NamingFace Fix
    
    * NameIpc Fix
    
    * NameFixIpc
    
    * LocaleBrainIpc
    
    * LocalePartsIpc
    
    * rework face menu
    
    * delete dot in ftl
    
    * add euuuuuula
    
    * IpcBrainTextureNew
    
    * CategoriesAndLocale
    
    * NoMeatIpc Fix
    
    * add increase draw rate battery at temp detla
    
    * doAfter and Damagetype healing change
    
    * MultiRepairableSystem SUPER Healing IPC
    
    * doAfter and healing change
    
    * neirogovno
    
    * EmoteSounds Fix
    
    * DeleteExcessLines
    
    * GibCellDrop
    
    * change drain action sprite
    
    * retrow is blunt (fix path)
    
    * MicroFixLocale
    
    * LocaleAccsessIpc
    
    * doAfterFaster and small healing
    
    * AddIpcDiagnosticHud
    
    * ADDBasicSurvivalLoadoutIpc
    
    * 1984
    
    * ADDEngineerSurvivalBox for IPC
    
    * ipccustomftlfix
    
    * ADDSurvivalBoxSecurity for Ipc
    
    * Add Hihich and Hahich survivalbox (clown and mime)
    
    * addnewfaces
    
    * add all faces to ingame menu
    
    * add locale, fix path and brain inhand sprite
    
    * Locale for SurvivalBoxIpc
    
    * SurvivalBoxMedical for IPC
    
    * RobustToolbox Upstream
    
    * yml1
    
    * markings fix
    
    * body species ipc
    
    * ipc brain and eyes
    
    * mob ipc species
    
    * parts torso and head fix
    
    * delete glowstick error
    
    * multi fix (need test)
    
    * delete face menu ipc
    
    * yml and cs fix (without temper)
    
    * crash loadout after merge conflict fix
    
    * After merge conflict fix v2
    
    * Loadout IPC med back
    
    * loadout fix again
    
    * Delete Internals (need test)
    
    * flashlight return
    
    * move update alert to client side
    
    * fix alert update mistake
    
    * Mozgi update
    
    * Karmashki fix
    
    * remove popup on IPC
    
    * NO YOGG Roundstart
    
    * Ipc guidebook
    
    * Guidebook ipc change
    
    * fix drain bug
    
    * SetDrawRate to PowerCellSystemDraw and fix temp react
    
    * fix ftl
    
    * add eula
    
    * add summary and some fix
    
    * Update Resources/Prototypes/SS220/Damage/containers.yml
    
    Co-authored-by: Anri <elyasin.ane@mail.ru>
    
    * checkout loadout ipc-master
    
    * change loadout
    
    * delete excess desc and suffix
    
    * ftl emergency boxes change
    
    * checkout again ipc-master
    
    * ADD loadout again
    
    * ipc speech emote new folder
    
    * remove readonly
    
    * delete normal temp const
    
    * new actions IPC for resprite
    
    * add icon for actions
    
    * fonarik state fix
    
    * change guidebook and add sound buzz/sneeze
    
    * Change volume
    
    * shlyapa fix
    
    ---------
    
    Co-authored-by: RetroCringeW <trblgomer@gmail.com>
    Co-authored-by: Anri <elyasin.ane@mail.ru>

---
+++ Content.Server/SS220/Ipc/IpcSystem.cs
@@ -0,0 +1,259 @@
+﻿// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+// Original code taken from: Corvax https://github.com/space-syndicate/space-station-14
+
+using Content.Shared.Actions;
+using Content.Shared.SS220.Ipc;
+using Content.Shared.Ninja.Components;
+using Content.Shared.Ninja.Systems;
+using Content.Shared.Popups;
+using Content.Shared.PowerCell;
+using Content.Shared.PowerCell.Components;
+using Content.Shared.Power.EntitySystems;
+using Content.Shared.Damage;
+using Content.Shared.Damage.Systems;
+using Content.Shared.Damage.Components;
+using Content.Shared.Emp;
+using Content.Shared.Movement.Systems;
+using Content.Shared.Mobs;
+using Content.Shared.Mobs.Systems;
+using Content.Shared.Mobs.Components;
+using Content.Shared.Sound.Components;
+using Content.Shared.UserInterface;
+using Content.Shared.Temperature;
+using Content.Shared.MagicMirror;
+using Content.Shared.Power;
+using Content.Server.Body.Components;
+
+namespace Content.Server.SS220.Ipc;
+
+public sealed partial class IpcSystem : EntitySystem
+{
+    [Dependency] private SharedActionsSystem _action = default!;
+    [Dependency] private SharedBatteryDrainerSystem _batteryDrainer = default!;
+    [Dependency] private SharedPopupSystem _popup = default!;
+    [Dependency] private PowerCellSystem _powerCell = default!;
+    [Dependency] private SharedBatterySystem _battery = default!;
+    [Dependency] private DamageableSystem _damageableSystem = default!;
+    [Dependency] private MovementSpeedModifierSystem _movementSpeedModifier = default!;
+    [Dependency] private MobStateSystem _mobState = default!;
+    [Dependency] private SharedUserInterfaceSystem _ui = default!;
+    [Dependency] private MobThresholdSystem _mobThresholdSystem = default!;
+
+    private static readonly LocId IpcDrainReady = "ipc-drain-enabled";
+    private static readonly LocId IpcDrainDisabled = "ipc-drain-disabled";
+    private static readonly LocId IpcNoBattery = "ipc-no-battery";
+    private static readonly TimeSpan SoundMinInterval = TimeSpan.FromSeconds(15);
+    private static readonly TimeSpan SoundMaXInterval = TimeSpan.FromSeconds(30);
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<IpcComponent, MapInitEvent>(OnMapInit);
+        SubscribeLocalEvent<IpcComponent, ComponentShutdown>(OnComponentShutdown);
+        SubscribeLocalEvent<IpcComponent, PowerCellChangedEvent>(OnPowerCellChanged);
+        SubscribeLocalEvent<IpcComponent, ToggleDrainActionEvent>(OnToggleDrainAction);
+        SubscribeLocalEvent<IpcComponent, EmpPulseEvent>(OnEmpPulse);
+        SubscribeLocalEvent<IpcComponent, RefreshMovementSpeedModifiersEvent>(OnRefreshMovementSpeedModifiers);
+        SubscribeLocalEvent<IpcComponent, MobStateChangedEvent>(OnMobStateChanged);
+        SubscribeLocalEvent<IpcComponent, OpenIpcFaceActionEvent>(OnOpenIpcFaceAction);
+        SubscribeLocalEvent<IpcComponent, DamageChangedEvent>(OnDamageChanged);
+        SubscribeLocalEvent<IpcComponent, BatteryStateChangedEvent>(OnBatteryStateChanged);
+        SubscribeLocalEvent<IpcComponent, OnTemperatureChangeEvent>(OnTemperatureChange);
+    }
+
+    private void OnMapInit(Entity<IpcComponent> ent, ref MapInitEvent args)
+    {
+        _action.AddAction(ent, ref ent.Comp.DrainBatteryActionEntity, ent.Comp.DrainBatteryAction);
+        _action.AddAction(ent, ref ent.Comp.ChangeFaceActionEntity, ent.Comp.ChangeFaceAction);
+        _movementSpeedModifier.RefreshMovementSpeedModifiers(ent.Owner);
+        Dirty(ent);
+    }
+
+    private void OnComponentShutdown(Entity<IpcComponent> ent, ref ComponentShutdown args)
+    {
+        _action.RemoveAction(ent.Owner, ent.Comp.DrainBatteryActionEntity);
+        _action.RemoveAction(ent.Owner, ent.Comp.ChangeFaceActionEntity);
+    }
+
+    /// <summary>
+    /// If the battery is present but discharged, the movement speed is reduced.
+    /// </summary>
+    private void OnBatteryStateChanged(Entity<IpcComponent> ent, ref BatteryStateChangedEvent args)
+    {
+        if (Terminating(ent))
+            return;
+
+        _movementSpeedModifier.RefreshMovementSpeedModifiers(ent.Owner);
+    }
+
+    /// <summary>
+    /// The presence of a battery affects the movement speed
+    /// and the ability to use the drain ability.
+    /// When removing the battery, forcibly disable the drain ability.
+    /// </summary>
+    private void OnPowerCellChanged(Entity<IpcComponent> ent, ref PowerCellChangedEvent args)
+    {
+        if (Terminating(ent))
+            return;
+
+        _movementSpeedModifier.RefreshMovementSpeedModifiers(ent.Owner);
+
+        if (_powerCell.HasBattery(ent.Owner))
+            return;
+
+        ent.Comp.DrainActivated = false;
+        _action.SetToggled(ent.Comp.DrainBatteryActionEntity, ent.Comp.DrainActivated);
+
+        // show popup only if comp removed
+        if (RemComp<BatteryDrainerComponent>(ent))
+        {
+            _popup.PopupEntity(Loc.GetString(IpcDrainDisabled), ent, ent);
+        }
+
+        Dirty(ent);
+    }
+
+    /// <summary>
+    /// Enable/disable the drain ability. 
+    /// </summary>
+    private void OnToggleDrainAction(Entity<IpcComponent> ent, ref ToggleDrainActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        if (!_powerCell.HasBattery(ent.Owner))
+        {
+            _popup.PopupEntity(Loc.GetString(IpcNoBattery), ent, ent);
+            return;
+        }
+
+        ent.Comp.DrainActivated = !ent.Comp.DrainActivated;
+        _action.SetToggled(ent.Comp.DrainBatteryActionEntity, ent.Comp.DrainActivated);
+        args.Handled = true;
+
+        if (ent.Comp.DrainActivated && _powerCell.TryGetBatteryFromSlot(ent.Owner, out var battery))
+        {
+            EnsureComp<BatteryDrainerComponent>(ent);
+            _batteryDrainer.SetBattery(ent.Owner, battery);
+        }
+        else
+            RemComp<BatteryDrainerComponent>(ent);
+
+        var message = ent.Comp.DrainActivated ? IpcDrainReady : IpcDrainDisabled;
+        _popup.PopupEntity(Loc.GetString(message), ent, ent);
+
+        Dirty(ent);
+    }
+
+    /// <summary>
+    /// The movement speed of the IPS is reduced if they are out of charge.
+    /// </summary>
+    private void OnRefreshMovementSpeedModifiers(Entity<IpcComponent> ent, ref RefreshMovementSpeedModifiersEvent args)
+    {
+        if (!_powerCell.TryGetBatteryFromSlot(ent.Owner, out var battery) || _battery.GetCharge(battery.Value.AsNullable()) / battery.Value.Comp.MaxCharge < 0.01f)
+        {
+            args.ModifySpeed(ent.Comp.LowChargeSpeed);
+        }
+    }
+
+    /// <summary>
+    /// The screen image change function is based on the Magic Mirror.
+    /// </summary>
+    private void OnOpenIpcFaceAction(Entity<IpcComponent> ent, ref OpenIpcFaceActionEvent args)
+    {
+        if (args.Handled)
+            return;
+
+        if (!HasComp<MagicMirrorComponent>(ent))
+            return;
+
+        // User open UI event.
+        var ev = new BeforeActivatableUIOpenEvent(args.Performer);
+
+        // Raise event on ent. MagicMirrorSystem call UpdateInterface().
+        RaiseLocalEvent(ent.Owner, ev);
+
+        // Open magic mirror UI
+        args.Handled = _ui.TryOpenUi(ent.Owner, MagicMirrorUiKey.Key, args.Performer);
+    }
+
+    /// <summary>
+    /// IPS take damage from EMP.
+    /// </summary>
+    private void OnEmpPulse(Entity<IpcComponent> ent, ref EmpPulseEvent args)
+    {
+        args.Affected = true;
+
+        var damage = new DamageSpecifier();
+        damage.DamageDict.Add("Shock", ent.Comp.DamageFromEmp);
+        _damageableSystem.TryChangeDamage(ent.Owner, damage);
+
+    }
+
+    /// <summary>
+    /// Organic beings in critical condition make sounds of labored breathing. IPS - beeps.
+    /// </summary>
+    private void OnMobStateChanged(Entity<IpcComponent> ent, ref MobStateChangedEvent args)
+    {
+        if (args.NewMobState is MobState.Critical)
+        {
+            var sound = EnsureComp<SpamEmitSoundComponent>(ent);
+            sound.Sound = ent.Comp.CritStateSound;
+            sound.MinInterval = SoundMinInterval;
+            sound.MaxInterval = SoundMaXInterval;
+        }
+        else
+        {
+            RemComp<SpamEmitSoundComponent>(ent);
+        }
+
+    }
+
+    /// <summary>
+    /// IPS easily return from a dead state to a critical state if they are repaired.
+    /// </summary>
+    private void OnDamageChanged(Entity<IpcComponent> ent, ref DamageChangedEvent args)
+    {
+        if (!TryComp<MobStateComponent>(ent, out var mobComp))
+            return;
+
+        if (!_mobState.IsDead(ent, mobComp))
+            return;
+
+        if (!TryComp<DamageableComponent>(ent, out var damageableComp))
+            return;
+
+        if (!_mobThresholdSystem.TryGetDeadThreshold(ent, out var threshold))
+            return;
+
+        if (_damageableSystem.GetTotalDamage(ent.Owner) > threshold)
+            return;
+
+        _mobState.ChangeMobState(ent, MobState.Critical);
+    }
+
+    /// <summary>
+    /// IPC are sensitive to temperature changes. 
+    /// Going into space without a hardsuit? The battery drains faster.
+    /// </summary>
+    private void OnTemperatureChange(Entity<IpcComponent> ent, ref OnTemperatureChangeEvent args)
+    {
+        if (!TryComp<PowerCellDrawComponent>(ent, out var draw))
+            return;
+
+        if (!TryComp<ThermalRegulatorComponent>(ent, out var regulator))
+            return;
+
+        var delta = Math.Abs(args.CurrentTemperature - regulator.NormalBodyTemperature);
+
+        float newDrawRate = ent.Comp.BaseDrawRate;
+
+        if (delta > ent.Comp.CritDelta)
+            newDrawRate = ent.Comp.CritDrawRate;
+        else if (delta > ent.Comp.OverDelta)
+            newDrawRate = ent.Comp.OverDrawRate;
+
+        _powerCell.SetDrawRate((ent.Owner, draw), newDrawRate);
+    }
+}
