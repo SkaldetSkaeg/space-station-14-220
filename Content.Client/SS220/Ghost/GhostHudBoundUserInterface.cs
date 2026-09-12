bcdaad35f5c05d449a4f8684173fe6e7c967cc7f
SkaldetSkaeg <impotekh@gmail.com>
2026-09-12 11:55:16 +0700

    + license

--- Content.Client/SS220/Ghost/GhostHudBoundUserInterface.cs
+++ Content.Client/SS220/Ghost/GhostHudBoundUserInterface.cs
@@ -1,3 +1,5 @@
+// © SS220, An EULA/CLA with a hosting restriction, full text: https://raw.githubusercontent.com/SerbiaStrong-220/space-station-14/master/CLA.txt
+
 using Content.Shared.SS220.Ghost;
 using JetBrains.Annotations;
 using Robust.Client.UserInterface;
